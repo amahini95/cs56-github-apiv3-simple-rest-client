a8ed6fd7d29cfeca0f2e3b9fb444e514654f33ad
Jonathan Easterman <jonathaneasterman@gmail.com>
2016-02-11 17:27:36 -0800

    Add roster file and GitHubUCSB class

--- src/edu/ucsb/cs56/projects/github/apiv3_simple_rest_client/GitHubUCSB.java
+++ src/edu/ucsb/cs56/projects/github/apiv3_simple_rest_client/GitHubUCSB.java
@@ -2,8 +2,11 @@ package edu.ucsb.cs56.projects.github.apiv3_simple_rest_client;
 
 import org.kohsuke.github.*;
 import java.util.*;
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.Charset;
 
-
+import java.nio.file.StandardOpenOption;
+import java.nio.*;
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,6 +19,7 @@ import java.util.Scanner;
 
 public class GitHubUCSB {
   public static GHMyself me;
+  public static GitHub github;
   public static List<GHRepository> repos;
   public static List<String> names;
   public static String rosterName = "roster.csv";
@@ -28,7 +32,8 @@ public class GitHubUCSB {
       // username = sc.nextLine();
       // System.out.println("Please enter your GitHub UCSB password: ");
       // password = sc.nextLine();
-      me = GitHub.connectToEnterprise("https://github.ucsb.edu/api/v3/","jdogg5566", "Whinfrey1").getMyself();
+      github = GitHub.connectToEnterprise("https://github.ucsb.edu/api/v3/","jdogg5566", "Whinfrey1");
+      me = github.getMyself();
       repos = me.listRepositories(50).asList();
       initNames();
     } catch(IOException e) {
@@ -43,9 +48,13 @@ public class GitHubUCSB {
     try {
 
       br = new BufferedReader(new FileReader(rosterName));
+      int i = 0;
       while ((line = br.readLine()) != null) {
-        String[] columns = line.split(",");
-        names.add(columns[5]);
+        if (i != 0){
+          String[] columns = line.split(",");
+          names.add(columns[5]);
+        }
+        i++;
       }
 
     } catch (FileNotFoundException e) {
@@ -66,22 +75,59 @@ public class GitHubUCSB {
 
   public static void main(String[] args) {
     try {
-      for(String name : names){
-        for(GHRepository repo : repos) {
-          Set<String> collaborators = repo.getCollaboratorNames();
-          if (collaborators.contains(name)) {
-            // Here
-            System.out.println("<p>" + name + "<br>" + repo.getName() + "</p>");
-            //GHContent repo.getReadme();
-            String url = "http://www.cs.ucsb.edu/~" + name + "/cs56/lab00/javadoc";
-            System.out.println("<p><a href='" + url + "'>Javadoc Link</a></p>");
-            List<GHCommit> commits = repo.listCommits().asList();
-            for (GHCommit commit : commits) {
-              System.out.println(commit.getCommitShortInfo().getMessage());
-            }
-            // do stuff
-          }
+      GHRepository lab00;
+      String readMe = new String();
+      Path file = Paths.get("output.html");
+      List<GHCommit> commits = new ArrayList<GHCommit>();
+      Set<String> exempt = new HashSet(Arrays.asList("jrcryan", "grinta", "jdogg5566", "pconrad", "arda", "chall01", "vincenicoara",
+      "allisonshedden", "omeedrabani", "hanna", "mbahia"));
+       ArrayList<String> urls = new ArrayList();
+      for (String name : names) {
+        String html = "<h1>" + name + "</h1><h2>cs56-w16-lab00</h2>";
+        GHUser user = github.getUser(name);
+        if (user == null) {
+          user = github.getUser("samuelechu");
+          html += "Error with Username";
         }
+        Map<String, GHRepository> repos = user.getRepositories();
+        lab00 = repos.get("cs56-w16-lab00");
+        if (lab00 == null) {
+          lab00 = repos.get("cs56-w15-lab00-");//samuelechu
+          html += "ERROR WITH REPO NAME";
+        }
+        if(lab00 == null) {
+          lab00 = repos.get("cs56-w15-lab00");//Kevin Lee
+          html += "ERROR WITH REPO NAME";
+        }
+        
+        try{
+          readMe = IOUtils.toString(lab00.getReadme().read());
+          String javadoc = readMe.substring(readMe.indexOf(":") + 2, readMe.indexOf("/javadoc")+8);
+          html += "<p><a href='" + javadoc + "'>Javadoc Link</a></p>";
+          commits = lab00.listCommits().asList();
+
+        } catch(StringIndexOutOfBoundsException e) {
+          html += "INVALID JAVADOC LINK";
+        }
+        catch(NullPointerException e) {
+          html += "Lab00 does not exist or is private";
+          readMe = "null";
+          commits = new ArrayList<GHCommit>();
+        }
+        //Set<String> collaborators = lab00.getCollaboratorNames();
+        html += "<ol>";
+
+        for (GHCommit commit : commits)
+          html += "<li>" + commit.getCommitShortInfo().getMessage() + "</li>";
+        
+        html += "</ol><h2>ReadMe Contents</h2><p>";
+        html += readMe;
+        html += "</p><br><br>";
+
+        ArrayList al = new ArrayList();
+        al.add(html);
+        Files.write(file, al, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+        System.out.println(html);
       }
     } catch(IOException e) {
       e.printStackTrace();
